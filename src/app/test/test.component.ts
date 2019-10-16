32494823ad67c672e87a0722f70520930c393634
Thibault <t.couloumy@gmail.com>
2019-10-16 20:14:35 +0200

    Test toggle button

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.sass']
+	selector: 'app-test',
+	templateUrl: './test.component.html',
+	styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+	constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	toggleSalon() {
+		
+		return this.http.post('http://localhost:3000/light/toggle', {}).subscribe((response) => {
+			console.log(response);
+		},
+		(error) => {
+			console.log(error);
+		});
+	}
 
 }
