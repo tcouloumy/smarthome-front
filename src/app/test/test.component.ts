95207fa52b0fb93e8551d1f4aa6dd9b3e982bf1c
Thibault <t.couloumy@gmail.com>
2020-03-26 17:13:05 +0100

    Add brightness et color controll

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,14 +8,50 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class TestComponent implements OnInit {
 
-	constructor(private http: HttpClient) { }
-
-	ngOnInit() {
+	brightness: number = 80;
+	color: object = {
+		r: 255,
+		g: 255,
+		b: 255
 	}
+
+	constructor(private http: HttpClient) {}
+
+	ngOnInit() {}
 
 	toggleSalon() {
 		
 		return this.http.post('http://79.83.169.199:3000/light/1/toggle', {}).subscribe((response) => {
+			console.log(response);
+		},
+		(error) => {
+			console.log(error);
+		});
+	}
+
+	changeBrightness(value) {
+
+		this.brightness = value;
+		
+		return this.http.post('http://79.83.169.199:3000/light/1/brightness', {
+			brightness: this.brightness
+		}).subscribe((response) => {
+			console.log(response);
+		},
+		(error) => {
+			console.log(error);
+		});
+	}
+
+	changeColor(r, g, b) {
+
+		this.color = {
+			r: r,
+			g: g,
+			b: b
+		}
+		
+		return this.http.post('http://79.83.169.199:3000/light/1/color', this.color).subscribe((response) => {
 			console.log(response);
 		},
 		(error) => {
